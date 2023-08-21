5ab5a91eace16511f3eda9a04439385053a89d59
“Abbosbek” <abbosbekqodirov2501@gmail.com>
2023-08-22 02:19:07 +0500

    header end

---
+++ components/Header.jsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+
+    const categories = [
+      { name: "Praktikum", slug: "praktikum" },
+      { name: "Projects", slug: "projects" }
+    ];
+
+  return (
+    <div className="container mx-auto  px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-500 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              Blog CMS
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import '@/styles/globals.scss'
+import { Layout } from '@/components'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout><Component {...pageProps} /></Layout>
 }
