086696bacafe7623dd7681355230b89edf887586
“Abbosbek” <abbosbekqodirov2501@gmail.com>
2023-08-22 15:09:22 +0500

    PostCard component created

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,17 @@ import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import { Categories, PostCard, PostWidget } from '@/components'
 
+import { getPosts } from '@/services'
+
+
 const posts = [
   { title: "MERN Stack Praktikum", excerpt: "MongoDB, ExpressJS, ReactJS, NodeJS" },
   { title: "Digital Marketing", excerpt: "ADS, Instagram, Telegram" }
 ]
 
-export default function Home() {
+export default function Home({ posts }) {
+
+  console.log(posts);
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -19,7 +24,7 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map(post =>(
-          <PostCard post={post} key={post.title}/>
+          <PostCard post={post.node} key={post.title}/>
           ))}
         </div>
 
@@ -32,4 +37,13 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+
+export async function getStaticProps(){
+  const posts = (await getPosts()) || [];
+
+  return{
+    props: {posts}
+  }
 }
